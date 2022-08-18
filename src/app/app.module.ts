b7dd8ac164564c760be8f1ba6cb8353d8b1fbc32
ShravyaRNadig <shravyyarnadig@gmail.com>
2022-08-18 21:04:23 +0530

    login homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommanComponent } from './comman/comman.component';
+import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommanComponent,
+    LoginComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
